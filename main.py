5a029ff4498ee1a74f1013f778a80be27f9cb24c
99210041580 <141840738+99210041580@users.noreply.github.com>
2024-03-26 04:30:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.6.33626.354
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Multicloud.Bot", "Multicloud.Bot\Multicloud.Bot.csproj", "{317F01F9-DBF2-4C50-885D-41406B9C1B34}"
+EndProject
+Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Multicloud.Common", "Multicloud.Common\Multicloud.Common.csproj", "{38E1EF12-EEAF-4836-AA84-BC63E1482177}"
+EndProject
+Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Multicloud.Models", "Multicloud.Models\Multicloud.Models.csproj", "{EAA85ED5-09DC-4225-851D-4D199573CE53}"
+EndProject
+Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Multicloud.Interfaces", "Multicloud.Interfaces\Multicloud.Interfaces.csproj", "{F56EE2F8-D397-45C8-BF61-AB937884E012}"
+EndProject
+Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Multicloud.Services", "Multicloud.Services\Multicloud.Services.csproj", "{270C5166-6DD7-42CC-B288-D0944B167B16}"
+EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Multicloud.Data", "Multicloud.Data\Multicloud.Data.csproj", "{3E983DE3-384A-4471-8966-29219EBC7B66}"
+EndProject
+Global
+  GlobalSection(SolutionConfigurationPlatforms) = preSolution
+    Debug|Any CPU = Debug|Any CPU
+    Release|Any CPU = Release|Any CPU
+  EndGlobalSection
+  GlobalSection(ProjectConfigurationPlatforms) = postSolution
+    {317F01F9-DBF2-4C50-885D-41406B9C1B34}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+    {317F01F9-DBF2-4C50-885D-41406B9C1B34}.Debug|Any CPU.Build.0 = Debug|Any CPU
+    {317F01F9-DBF2-4C50-885D-41406B9C1B34}.Release|Any CPU.ActiveCfg = Release|Any CPU
+    {317F01F9-DBF2-4C50-885D-41406B9C1B34}.Release|Any CPU.Build.0 = Release|Any CPU
+    {38E1EF12-EEAF-4836-AA84-BC63E1482177}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+    {38E1EF12-EEAF-4836-AA84-BC63E1482177}.Debug|Any CPU.Build.0 = Debug|Any CPU
+    {38E1EF12-EEAF-4836-AA84-BC63E1482177}.Release|Any CPU.ActiveCfg = Release|Any CPU
+    {38E1EF12-EEAF-4836-AA84-BC63E1482177}.Release|Any CPU.Build.0 = Release|Any CPU
+    {EAA85ED5-09DC-4225-851D-4D199573CE53}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+    {EAA85ED5-09DC-4225-851D-4D199573CE53}.Debug|Any CPU.Build.0 = Debug|Any CPU
+    {EAA85ED5-09DC-4225-851D-4D199573CE53}.Release|Any CPU.ActiveCfg = Release|Any CPU
+    {EAA85ED5-09DC-4225-851D-4D199573CE53}.Release|Any CPU.Build.0 = Release|Any CPU
+    {F56EE2F8-D397-45C8-BF61-AB937884E012}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+    {F56EE2F8-D397-45C8-BF61-AB937884E012}.Debug|Any CPU.Build.0 = Debug|Any CPU
+    {F56EE2F8-D397-45C8-BF61-AB937884E012}.Release|Any CPU.ActiveCfg = Release|Any CPU
+    {F56EE2F8-D397-45C8-BF61-AB937884E012}.Release|Any CPU.Build.0 = Release|Any CPU
+    {270C5166-6DD7-42CC-B288-D0944B167B16}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+    {270C5166-6DD7-42CC-B288-D0944B167B16}.Debug|Any CPU.Build.0 = Debug|Any CPU
+    {270C5166-6DD7-42CC-B288-D0944B167B16}.Release|Any CPU.ActiveCfg = Release|Any CPU
+    {270C5166-6DD7-42CC-B288-D0944B167B16}.Release|Any CPU.Build.0 = Release|Any CPU
+    {3E983DE3-384A-4471-8966-29219EBC7B66}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+    {3E983DE3-384A-4471-8966-29219EBC7B66}.Debug|Any CPU.Build.0 = Debug|Any CPU
+    {3E983DE3-384A-4471-8966-29219EBC7B66}.Release|Any CPU.ActiveCfg = Release|Any CPU
+    {3E983DE3-384A-4471-8966-29219EBC7B66}.Release|Any CPU.Build.0 = Release|Any CPU
+  EndGlobalSection
+  GlobalSection(SolutionProperties) = preSolution
+    HideSolutionNode = FALSE
+  EndGlobalSection
+  GlobalSection(ExtensibilityGlobals) = postSolution
+    SolutionGuid = {A87CCECA-9769-42B4-B3FC-4E83CCC4DB56}
+  EndGlobalSection
+EndGlobal
